b3dbd127710d5302a74ca002f6fdac475b1ef194
Jessica Abels <jessabels92@gmail.com>
2026-07-05 14:56:33 -0700

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Layout from './components/layout/Layout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import MyBooks from './pages/MyBooks';
 
 function App() {
   return (
-    <div className="App">
-       <Layout/>
-    </div>
-  );
+    <BrowserRouter>
+  <Routes>
+    <Route element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/books" element={<MyBooks />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
+  )
 }
 
 export default App;
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -13,7 +13,7 @@ export default function Sidebar() {
   return (
     <div>
     <h1>My Reading Adventure</h1>
-    {navItems.map((navItem)=> <li>{navItem.label}</li>)}
+    {navItems.map((navItem)=> <li key={navItem.label}>{navItem.label}</li>)}
     </div>
   )
 }
