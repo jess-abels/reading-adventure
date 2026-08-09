650ab2e159f823e9a959ca9e3f42664d8ac6717e
Jessica Abels <jessabels92@gmail.com>
2026-08-09 13:50:23 -0700

    Create separate component for BookCard

---
+++ src/components/BookCard.tsx
@@ -0,0 +1,24 @@
+import  "./BookCard.css"
+
+export default function BookCard({filteredBooks}:any) {
+
+  return (
+    <div>
+        <ul className="book-card-container">
+{filteredBooks.map((book:any) =>  (
+
+      <li className="book-card">
+        <img src={book.cover}/>
+        <div className="book-info">
+        <h3>{book.title}</h3>
+        <p>{`By: ${book.author}`}</p>
+        <p>{book.description}</p>
+        </div>
+      </li>
+   
+))
+}   </ul>
+    
+    </div>
+  )
+}
--- src/pages/MyBooks.tsx
+++ src/pages/MyBooks.tsx
@@ -1,5 +1,5 @@
-import  "./MyBooks.css"
 import {books} from  "../data/books";
+import BookCard from "../components/BookCard";
 
 export default function MyBooks() {
   const wantToRead = books.filter((book) => book.status === "wantToRead");
@@ -8,49 +8,14 @@ export default function MyBooks() {
   return (
     <div>
     <h1>My Books</h1>
-    <h3>Want to Read</h3>
-    <ul className="book-card-container">
-{wantToRead.map((book) =>  (
-      <li className="book-card">
-        <img src={book.cover}/>
-        <div className="book-info">
-        <h3>{book.title}</h3>
-        <p>{`By: ${book.author}`}</p>
-        <p>{book.description}</p>
-        </div>
-      </li>
-))
-}
-    </ul>
-    <h3>Currently Reading</h3>
-        <ul className="book-card-container">
-{currentlyReading.map((book) =>  (
-      <li className="book-card">
-        <img src={book.cover}/>
-        <div className="book-info">
-        <h3>{book.title}</h3>
-        <p>{`By: ${book.author}`}</p>
-        <p>{book.description}</p>
-        </div>
-      </li>
-))
-}
-    </ul>
 
-    <h3>Finished</h3>
-        <ul className="book-card-container">
-{finishedReading.map((book) =>  (
-      <li className="book-card">
-        <img src={book.cover}/>
-        <div className="book-info">
-        <h3>{book.title}</h3>
-        <p>{`By: ${book.author}`}</p>
-        <p>{book.description}</p>
-        </div>
-      </li>
-))
-}
-    </ul>
+    <h3>Want to Read</h3>
+    <BookCard filteredBooks={wantToRead}/>
+     <h3>Currently Reading</h3>
+    <BookCard filteredBooks={currentlyReading}/>
+     <h3>Finished</h3>
+     <BookCard filteredBooks={finishedReading}/>
+     
     </div>
   )
 }
