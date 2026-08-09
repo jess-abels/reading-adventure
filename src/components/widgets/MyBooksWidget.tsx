d3eb33008100b7fcb63a3cbb48a6b631ffa00939
Jessica Abels <jessabels92@gmail.com>
2026-08-09 11:34:01 -0700

    Add some styles to Books Widget

--- src/components/widgets/MyBooksWidget.tsx
+++ src/components/widgets/MyBooksWidget.tsx
@@ -1,13 +1,14 @@
 import  "./MyBooksWidget.css"
 
 export default function MyBooksWidget() {
+
   return (
     <div>
     <h1>My Books</h1>
     <div className="book-categories">
-      <div className="category-card">Want To Read</div>
-      <div className="category-card">Currently Reading</div>
-      <div className="category-card">Finished</div>
+      <div className="category-card yellow">Want To Read</div>
+      <div className="category-card blue">Currently Reading</div>
+      <div className="category-card green">Finished</div>
    </div>
     </div>
   )
