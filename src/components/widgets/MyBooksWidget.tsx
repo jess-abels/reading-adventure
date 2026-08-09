cfc1b723afaaab60d74dab6da3f0d242fcf45a0e
Jessica Abels <jessabels92@gmail.com>
2026-08-09 12:28:54 -0700

    Display number of books want to read, currently reading, and finished

--- src/components/widgets/MyBooksWidget.tsx
+++ src/components/widgets/MyBooksWidget.tsx
@@ -1,14 +1,24 @@
 import  "./MyBooksWidget.css"
+import {books} from  "../../data/books";
 
 export default function MyBooksWidget() {
+    const numWantToRead = books.filter((book) => book.status === "wantToRead").length;
+    const currentlyReading = books.filter((book) => book.status === "currentlyReading").length;
+    const finishedReading = books.filter((book) => book.status === "finishedReading").length;
 
   return (
     <div>
     <h1>My Books</h1>
     <div className="book-categories">
-      <div className="category-card yellow">Want To Read</div>
-      <div className="category-card blue">Currently Reading</div>
-      <div className="category-card green">Finished</div>
+      <div className="category-card yellow">Want To Read
+        <p>{numWantToRead} books</p>
+      </div>
+      <div className="category-card blue">Currently Reading
+        <p>{currentlyReading} books</p>
+      </div>
+      <div className="category-card green">Finished
+        <p>{finishedReading} books</p>
+      </div>
    </div>
     </div>
   )
